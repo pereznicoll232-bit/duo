b76817730193de25290c42cdf49e192066451f69
sara perez <pereznicoll232@gmail.com>
2026-02-24 23:23:14 -0500

    pregunta

--- public/index.php
+++ public/index.php
@@ -3,4 +3,5 @@ require_once __DIR__ . '/../config/config.php';
 echo 'Proyecto iniciado 🚀';
 echo 'Hola Sara Nicol, ¿Como estas?';
 echo "como estas ? ";
-echo "Bien y tu?"
+echo "Bien y tu?";
+echo "que haces? ";
