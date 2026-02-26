8543ccb05d630f32117e2547f0b3c424d1d86859
sara perez <pereznicoll232@gmail.com>
2026-02-26 16:35:19 -0500

    trabajo adelantado

---
+++ public/pages/dashboard.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PH SARA PEREZ</title>
+</head>
+<body>
+    <div> 
+        <div>
+            <p>PH.SARA_PEREZ</p>
+        </div>
+        <div>
+            <div>
+                <p>SOBRE MI</p>
+            </div>
+            <div>
+                <p>PORTAFOLIO</p>
+            </div>
+            <div>
+                <p>SERVICIOS</p>
+            </div>
+            <div>
+                <p>ANUNCIOS</p>
+            </div>
+        </div>
+        <div>
+            <button>RESERVAR SESIÓN</button>
+        </div>
+    </div>
+    <div>
+        <div>
+            <div>
+                <p>FOTOGRAFÍA</p>
+            </div>
+            <div>
+                <p>MOTOVELOCIDAD</p>
+            </div>
+            <div>
+                <p>Soy Sara Peréz , Fotógrafa especializada en motos , circuitos y motovelocidad. Capturo la adrenalina, la rapidéz y la belleza de cada máquina en movimiento</p>
+            </div>
+            <div>
+                <button>VER PORTAFOLIO</button>
+            </div>
+
+        </div>
+        <div>
+            <div>
+                <p>4+</p>
+                <p>Años y experiencia</p>
+            </div>
+            <div>
+                <P>200+</P>
+                <p>Eventos Cubiertos</p>
+            </div>
+            <div>
+                <p>12k</p>
+                <p>Fotografías ent</p>
+            </div>
+        </div>
+
+        
+    </div>
+</body>
+</html>
